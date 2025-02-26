a2ebf9d8479dbe60ed9a67bc6c205782098ae3a5
Malikaelabderrahmani <malikarahmani67@gmail.com>
2025-02-26 16:32:39 +0000

    update

--- rhms/src/main/java/pfe/mandomati/rhms/Controller/EmployeeController.java
+++ rhms/src/main/java/pfe/mandomati/rhms/Controller/EmployeeController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 //import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
-//@RequestMapping("/api")
+@RequestMapping("/hey")
 public class EmployeeController {
 
     @GetMapping("/hello")
